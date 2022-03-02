175e27b096230cdac338e39c662500d0ef9a660c
Nikola Jovanović <nidza.jov@gmail.com>
2022-03-02 20:42:37 +0100

    Initial commit

---
+++ src/components/ClearButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button v-on:click="clearSentence">Clear</button>
+</template>
+
+<script>
+export default {
+  name: 'ClearButton',
+  data () {
+    return {
+      totalClicks: 0
+    }
+  },
+  methods: {
+    clearSentence () {
+      this.totalClicks += 1
+      this.$emit('button-clicked', { clicks: this.totalClicks })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
