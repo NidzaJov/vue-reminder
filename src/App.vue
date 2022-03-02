d43b2576312536b700dcfde420ac150b7ee8a47c
Nikola Jovanović <nidza.jov@gmail.com>
2022-03-02 22:07:01 +0100

    Demo end experiment with v-model, v-bind, v-on etc.

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,35 @@
   <div id="app">
     Hello, Vue!
     <ReactiveInput />
+    <custom-button :myCount="count" @click="incrementCount"></custom-button>
+    <input type="text" v-model="message">
+    <input type="checkbox" v-model="check">
+    <input type="text" v-bind:value="message">
   </div>
 </template>
 
 <script>
 
 import ReactiveInput from './components/ReactiveInput.vue'
+import CustomButton from './components/CustomButton.vue'
 
 export default {
   name: 'App',
   components: {
-    ReactiveInput
+    ReactiveInput,
+    CustomButton
+  },
+  data () {
+    return {
+      count: 0,
+      message: 'Testing Vue',
+      check: false
+    }
+  },
+  methods: {
+    incrementCount (eventData) {
+      this.count += eventData.amount
+    }
   }
 }
 </script>
---
+++ src/components/CustomButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="custom-button" @click="handleClick">
+      <span class="button-text">
+          Klikni me: {{doubleCount}}
+      </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'CustomButton',
+  props: {
+    myCount: {
+      type: Number
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$emit('click', { amount: 1 })
+    }
+  },
+  computed: {
+    doubleCount () {
+      return this.myCount * 10
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@button-color: #333333;
+@text-color: #EEEEEE;
+
+.custom-button {
+    background-color: @button-color;
+    .button-text {
+        color: @text-color
+    }
+}
+
+</style>
